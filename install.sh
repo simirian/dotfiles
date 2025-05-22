7311a365b1e799168a614fb7e47e2e274dfdc86d
simirian <simirian5813@gmail.com>
2025-05-21 23:26:48 -0400

    feat: better install behavior
    Backs up existing files instead of just overwriting them. Also no longer
    follows links.

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ path="$PWD/$0"
 
 install() {
   echo "installing $1 to $2"
-  rm -r $2 &>/dev/null && echo "removed existing file at $2"
-  ln -s --backup=existing "${path%/*}/$1" "$2"
+  ln -ns --backup=existing "${path%/*}/$1" "$2"
 }
 
 install bashrc ~/.bashrc
