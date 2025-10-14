34e0980ac27df3ad06a7409ae40c9cd1372f3bcc
simirian <simirian5813@gmail.com>
2025-10-13 22:55:57 -0400

    feat: added fuzzel configuration

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 if [[ $HOME == /root* ]]; then
-	echo "Do not run this script as root!"
-	exit 1
+  echo "Do not run this script as root!"
+  exit 1
 fi
+
+help() {
+  echo 'Usage: ./install.sh [OPTIONS]'
+  echo 'Install dotfiles to their correct locations in the user configuration.'
+  echo ''
+  echo 'Options:'
+  echo '  -b --backup=[CONTROL] attempt to back up existing files, see `man 1 ln`'
+  echo '  -p --pkg              install required packages in `pkglist.txt`'
+  echo '  -d --dry-run          simulate installation, but don''t install anything'
+  echo '  -h --help             print this help message and exit'
+}
 
 script=$(which $0)
 cfg=${XDG_CONFIG_HOME-$HOME/.config}
@@ -13,31 +24,36 @@ flags=("-fsT")
 dry=""
 pkg=""
 while [[ -n $1 ]]; do
-	case $1 in
-		--backup*)
-			flags+=($1)
-			;;
-		--pkg)
-			pkg=true
-			;;
-		--dry-run)
-			dry=true
-			;;
-		*)
-			echo "Unknown option: $1"
-			exit 1
-			;;
-	esac
-	shift
+  case $1 in
+    -b | --backup*)
+      flags+=($1)
+      ;;
+    -p | --pkg)
+      pkg=true
+      ;;
+    -d | --dry-run)
+      dry=true
+      ;;
+    -h | --help)
+      help
+      exit 0
+      ;;
+    *)
+      echo "Unknown option: $1"
+      help
+      exit 1
+      ;;
+  esac
+  shift
 done
 
 if [[ -n $pkg ]]; then
-	echo "Attempting to install packages with pacman; sudo will ask for credentials."
-	sudo pacman -Syu --needed - < "${script%/*}/pkglist.txt"
-	if [[ $? == 1 ]]; then
-		echo "Failed to install packages, exiting."
-		exit 1
-	fi
+  echo "Attempting to install packages with pacman; sudo will ask for credentials."
+  sudo pacman -Syu --needed - < "${script%/*}/pkglist.txt"
+  if [[ $? == 1 ]]; then
+    echo "Failed to install packages, exiting."
+    exit 1
+  fi
 fi
 
 declare -A targets
@@ -47,12 +63,13 @@ targets[tmux]="$cfg/tmux"
 targets[bashrc]="$HOME/.bashrc"
 targets[gitconfig]="$HOME/.gitconfig"
 targets[nvim]="$cfg/nvim"
+targets[fuzzel]="$cfg/fuzzel"
 
 for src in ${!targets[@]}; do
-	msg="Installing $src to ${targets[$src]} ..."
-	echo $msg
-	if [[ -z $dry ]]; then
-		ln ${flags[@]} "${script%/*}/$src" "${targets[$src]}"\
-			&& echo -e "\e[1F$msg DONE"
-	fi
+  msg="Installing $src to ${targets[$src]} ..."
+  echo $msg
+  if [[ -z $dry ]]; then
+    ln ${flags[@]} "${script%/*}/$src" "${targets[$src]}"\
+      && echo -e "\e[1F$msg DONE"
+  fi
 done
